b689aec9626b78db22af92bde2e7ac9f84945b2c
Robert Sparks <robert.sparks@gousto.co.uk>
2022-02-19 21:07:47 +0000

    updated run command to be exposed to local network

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-FLASK_APP="server" poetry run python -m flask run > /dev/null 2>&1 & disown
+FLASK_APP="server" poetry run python -m flask run --host=0.0.0.0.0 > /dev/null 2>&1 & disown
 poetry run python main.py
